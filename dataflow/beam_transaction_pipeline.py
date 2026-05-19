7fb189f3bee7bbd7f72408aa7b25680b8597db96
Bhanu Teja Konduru <bhanuteja.us@gmail.com>
2026-05-19 15:56:48 -0400

    wrote small part of the need to code more.

--- dataflow/beam_transaction_pipeline.py
+++ dataflow/beam_transaction_pipeline.py
@@ -7,3 +7,24 @@ This pipeline will:
 3. Convert transaction amounts to USD.
 4. Write cleaned records to BigQuery.
 """
+
+
+CURRENCY_TO_USD = {
+    "USD": 1.00,
+    "EUR": 1.08,
+    "GBP": 1.27,
+    "INR": 0.012,
+    "CAD": 0.73,
+}
+
+BIGQUERY_SCHEMA = {
+    "fields": [
+        {"name": "transcation_id", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "user_id", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "transcation_timestamp", "type": "timestamp", "mode": "REQUIRED"},
+        {"name": "orginal_amout", "type": "NUMERIC", "mode": "REQUIRED"},
+        {"name": "currency", "type": "STRING", "mode": "REQUIRED"},
+        {"name": "amount_usd", "type": "NUMERIC", "mode": "REQUIRED"},
+        {"name": "ingestion_timestamp", "type": "NUMERIC", "mode": "REQUIRED"},
+    ]
+}
